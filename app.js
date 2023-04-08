e12a4a61eee297c48fc55e2f4c3d44332abc59bf
Fredericarolanda <114889285+Fredericarolanda@users.noreply.github.com>
2023-04-08 22:45:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Nama : Frederica Rolanda Phoandy NIM : 51020006 Jurusan : Sistem Informasi\n')
 })
 
 app.listen(PORT, HOST)
